68748e035bffe18ac518f183ad88af36b2b28bbc
DDBC23 <danielbc23@hotmail.com>
2022-03-31 18:27:07 +0000

    add header nav dropdown css and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+document.getElementById("modes-button").addEventListener("click", gameDrop);
+document.getElementById("rules").addEventListener("click", rulesDrop);
 
+function gameDrop (event) {
+    let gameModeList = document.getElementById("game-drop-content");
 
+    if(gameModeList.style.display ==="block") {
+        gameModeList.style.display ="none";
+    } else {
+        gameModeList.style.display ="block";
+    }
+}
 
+function rulesDrop (event) {
+    let rulesList = document.getElementById("rules-drop-content");
+
+    if(rulesList.style.display ==="block") {
+        rulesList.style.display ="none";
+    } else {
+        rulesList.style.display ="block";
+    }
+}
