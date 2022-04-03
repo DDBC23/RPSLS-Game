e48e79b2795644696976853555225e8e31b2c956
DDBC23 <danielbc23@hotmail.com>
2022-04-03 17:10:15 +0000

    Clean up dropdown menu and pop up window js and add rules window class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,48 @@
-document.getElementById("modes-button").addEventListener("click", gameDrop);
-
-/**either hides or shows game modes dropdown */
-function gameDrop(event) {
-    let gameModeList = document.getElementById("game-drop-content");
-
-    if (gameModeList.style.display === "block") {
-        gameModeList.style.display = "none";
-    } else {
-        gameModeList.style.display = "block";
-    }
-}
-
-window.addEventListener("click", function(event) {
-    let dropdownContent = document.getElementsByClassName("dropdown-content");
-    const rpslsRulesWindow = document.getElementById("rpsls-rules-window");
-    let rpslsRulesFull = rpslsRulesWindow.children[0, 1, 2];
-
-    if (event.target === rpslsRulesWindow) {
-        if (rpslsRulesWindow.style.display === "flex") {
-        rpslsRulesWindow.style.display = "none";
-        }
-    }
+/** shows or hides dropdown buttons and pop out windows based on event target */
+window.addEventListener("click", function (event) {
+    // dropdown content show or hide
+    const dropdownContent = document.getElementsByClassName("dropdown-content");
 
     if (event.target.classList.contains("menu-button") === false) {
         for (dropdown of dropdownContent) {
             dropdown.style.display = "none";
         }
     }
-})
+    
+    // rules dropdown show or hide
+    const rulesList = document.getElementById("rules-drop-content");
+    const rulesButton = document.getElementById("rules");
 
-document.getElementById("rules").addEventListener("click", rulesDrop);
-
-/**either hides or shows rules dropdown */
-function rulesDrop(event) {
-    let rulesList = document.getElementById("rules-drop-content");
-
-    if (rulesList.style.display === "block") {
+    if (event.target === rulesButton) {
+        if (rulesList.style.display = "none") {
+            rulesList.style.display = "block";
+        }
+    } else if (event.target !== rulesButton) {
         rulesList.style.display = "none";
-    } else {
-        rulesList.style.display = "block";
     }
-}
 
-document.getElementById("rpsls-rules").addEventListener("click", rpslsRules);
+    // modes dropdown show or hide
+    const gameModeList = document.getElementById("game-drop-content");
+    const modesButton = document.getElementById("modes");
 
-/**shows or closes rpsls rules window */
-function rpslsRules(event) {
-    let rpslsRulesWindow = document.getElementById("rpsls-rules-window");
+    if (event.target === modesButton) {
+        if (gameModeList.style.display = "none") {
+            gameModeList.style.display = "block";
+        }
+    } else if (event.target !== modesButton) {
+        gameModeList.style.display = "none";
+    }
 
-    if (rpslsRulesWindow.style.display === "flex") {
-        rpslsRulesWindow.style.display = "none";
-    } else {
+    // rpsls rules window show or hide
+    const rpslsRules = document.getElementById("rpsls-rules");
+    const rpslsRulesWindow = document.getElementById("rpsls-rules-window");
+
+    if (event.target === rpslsRules) {
         rpslsRulesWindow.style.display = "flex";
+    } else if (event.target !== rpslsRules) {
+        rpslsRulesWindow.style.display = "none";
     }
-}
+})
 
 let playerChoices = document.getElementsByClassName("rpsls-selector");
 
@@ -61,7 +50,7 @@ let playerChoices = document.getElementsByClassName("rpsls-selector");
 for (const playerChoice of playerChoices) {
     playerChoice.addEventListener("click", function playerSelection() {
         let playerSelected = document.getElementById("player-selection");
-        var playerPick ="";
+        var playerPick = "";
 
         if (this.getAttribute("data-type") === "rock-selector") {
             playerSelected.innerHTML = `<img src="/assets/images/rock.jpg" alt="icon for rock">
@@ -72,12 +61,12 @@ for (const playerChoice of playerChoices) {
             playerSelected.innerHTML = `<img src="/assets/images/paper.jpg" alt="icon for paper">
                 <figcaption>Ready to wrap this up?</figcaption>
                 <button id="player-confirm">Confirm choice</button>`;
-                playerPick = "paper";
+            playerPick = "paper";
         } else if (this.getAttribute("data-type") === "scissors-selector") {
             playerSelected.innerHTML = `<img src="/assets/images/scissors.jpg" alt="icon for scissors">
                 <figcaption>Will you make the cut?</figcaption>
                 <button id="player-confirm">Confirm choice</button>`;
-                playerPick = "scissors";
+            playerPick = "scissors";
         } else if (this.getAttribute("data-type") === "lizard-selector") {
             playerSelected.innerHTML = `<img src="/assets/images/lizard.jpg" alt="icon for lizard">
                 <figcaption>Ready to gecko-ing?</figcaption>
@@ -87,7 +76,7 @@ for (const playerChoice of playerChoices) {
             playerSelected.innerHTML = `<img src="/assets/images/spock.jpg" alt="icon for Spock">
                 <figcaption>Ready to Trek them out?</figcaption>
                 <button id="player-confirm">Confirm choice</button>`;
-                playerPick = "spock";
+            playerPick = "spock";
         }
         playerSelected.setAttribute("data-type", playerPick);
     })
